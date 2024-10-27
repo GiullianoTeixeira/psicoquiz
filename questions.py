0840d3f8afe429d885802277c008b26e03f6a850
GMT <giullianomteixeira@gmail.com>
2024-10-27 16:03:00 -0300

    fix: results page

--- questions.py
+++ questions.py
@@ -1,72 +1,72 @@
 questions = [
-    {"question": "Você prefere trabalhar:", "options": [
+    {"question": "Pergunta 1", "options": [
         {"text": "Opção 1", "type": "tipoA"},
         {"text": "Opção 2", "type": "tipoB"},
         {"text": "Opção 3", "type": "tipoC"},
         {"text": "Opção 4", "type": "tipoD"},
         {"text": "Opção 5", "type": "tipoE"}
     ]},
-    {"question": "Como você lida com o estresse?", "options": [
+    {"question": "Pergunta 2", "options": [
         {"text": "Opção 1", "type": "tipoA"},
         {"text": "Opção 2", "type": "tipoB"},
         {"text": "Opção 3", "type": "tipoC"},
         {"text": "Opção 4", "type": "tipoD"},
         {"text": "Opção 5", "type": "tipoE"}
     ]},
-    {"question": "Qual atividade você prefere?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Como você encara desafios?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Qual seu estilo de aprendizado preferido?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Como você toma decisões?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Qual é o seu ambiente de trabalho ideal?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Como você expressa sua criatividade?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "O que te motiva?", "options": [
-        {"text": "Opção 1", "type": "tipoA"},
-        {"text": "Opção 2", "type": "tipoB"},
-        {"text": "Opção 3", "type": "tipoC"},
-        {"text": "Opção 4", "type": "tipoD"},
-        {"text": "Opção 5", "type": "tipoE"}
-    ]},
-    {"question": "Como você gerencia seu tempo?", "options": [
+    {"question": "Pergunta 3", "options": [
         {"text": "Opção 1", "type": "tipoA"},
         {"text": "Opção 2", "type": "tipoB"},
         {"text": "Opção 3", "type": "tipoC"},
         {"text": "Opção 4", "type": "tipoD"},
         {"text": "Opção 5", "type": "tipoE"}
     ]}
+    #, {"question": "Pergunta 4", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 5", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 6", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 7", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 8", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 9", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]},
+    # {"question": "Pergunta 10", "options": [
+    #     {"text": "Opção 1", "type": "tipoA"},
+    #     {"text": "Opção 2", "type": "tipoB"},
+    #     {"text": "Opção 3", "type": "tipoC"},
+    #     {"text": "Opção 4", "type": "tipoD"},
+    #     {"text": "Opção 5", "type": "tipoE"}
+    # ]}
 ]
